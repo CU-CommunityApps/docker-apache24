9ee46e0a3791eaf3145d439a136f2971a3d8de6a
Scott Sorrentino <sms249@cornell.edu>
2017-08-10 15:38:34 -0400

    Update CUWA build script
    Bring script consistent with mechanism used in docker-apache22-official.

--- bin/build-mod_cuwebauth.sh
+++ bin/build-mod_cuwebauth.sh
@@ -1,37 +1,45 @@
 #!/bin/bash
 
-BUILD_TAG="dtr.cucloud.net/cs/apache24:cuwabuild-tmp"
-CONTAINER_NAME=${BUILD_TAG//[\/:.]}
-CUWA_VERSION="2.3.0.238"
-
 set -e
 
-[[ ! -z "${1}" ]] && CUWA_VERSION="${1}"
+for I in CUWA_VERSION DOCKER_IMG; do
+    [[ -z "${!I}" ]] && echo "Please define environment variable ${I}" && exit 1
+done
+
+BUILD_TAG="${DOCKER_IMG}:cuwabuild-tmp"
+CONTAINER_NAME=${BUILD_TAG//[\/:.]}
 
 # Make sure someone didn't cd into bin/, or call me from a completely different path
 BASEDIR=$(dirname "${0}")"/../"
 
-echo Updating mod_cuwebauth.so to version ${CUWA_VERSION}
+echo Creating build image for compiling mod_cuwebauth.so version ${CUWA_VERSION}
 
 set -x
+
+[[ -f ${BASEDIR}/lib/mod_cuwebauth.so ]] && rm ${BASEDIR}/lib/mod_cuwebauth.so
 
 docker build \
        --build-arg CUWA_VERSION=${CUWA_VERSION} \
        -f ${BASEDIR}Dockerfile.cuwa-build \
        --force-rm \
+       --no-cache \
        --pull \
        -t ${BUILD_TAG} \
        ${BASEDIR}
 
 # Will generate error if you already have a container named ${CONTAINER_NAME}
 # Most likely scenario would be a previous failed build attempt.
+echo Instantiating container to pull mod_cuwebauth.so artifact.
 docker create \
        --name ${CONTAINER_NAME} \
        ${BUILD_TAG}
 
 # Copy module from build container to the previously-determined base directory.
-docker cp ${CONTAINER_NAME}:/usr/lib/apache2/modules/mod_cuwebauth.so ${BASEDIR}lib/
+echo Copying mod_cuwebauth.so to local lib/ directory.
+[[ ! -d ${BASEDIR}/lib ]] && mkdir ${BASEDIR}/lib
+docker cp ${CONTAINER_NAME}:/usr/lib/apache2/modules/mod_cuwebauth.so ${BASEDIR}/lib/
 
 # Clean up
+echo Cleaning up build image and container.
 docker rm ${CONTAINER_NAME}
 docker rmi ${BUILD_TAG}
